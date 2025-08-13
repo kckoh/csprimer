b303f9bfa191ffa7921d1b2bbf9c01fdb9c11db5
KyungCheol (KC) Koh <43446639+kckoh@users.noreply.github.com>
2025-08-13 05:28:19 +0000

    implement the pangram with c

--- computer-systems/intro-c/fast-pangram/pangram.c
+++ computer-systems/intro-c/fast-pangram/pangram.c
@@ -6,24 +6,27 @@
 bool ispangram(char *s) {
   // TODO implement this!
   // loop through every character and find if it is in the ascii
-  printf("%s", s);
+
+  unsigned int bs = 0;
   while (*s != '\0'){
     unsigned int ch = (unsigned int) *s;
+    
     // 65 A 90: Z
     // 97:a 122: z
     // Upper case
     // convert it to lower case
     if (ch >= 65 && ch <= 90 ){
-      ch += 22;
+      ch += 32;
     }
     // do something here
+    // 
     if (ch >= 97 && ch <= 122 ){
-      
+      bs |= 1 << (ch - 'a');
     }
     // Lower case
     s++; // move to next character
   }
-  return false;
+  return bs == 0x03ffffff;
 }
 
 int main() {
