ab1f030035a3dbec3c84eaf025af4f9f8c51ebe4
KyungCheol (KC) Koh <43446639+kckoh@users.noreply.github.com>
2025-08-16 08:45:45 +0000

    temp save for hashmap.c

--- computer-systems/intro-c/README.md
+++ computer-systems/intro-c/README.md
@@ -2,5 +2,5 @@
 sudo apt update
 sudo apt install entr
 cd computer-systems/intro-c
-ls dynarray.c | entr sh -c 'gcc dynarray.c -o dynarray && ./dynarray'
+ls hashmap.c | entr sh -c 'gcc hashmap.c -o hashmap && ./hashmap'
 ls test.c | entr sh -c 'gcc test.c -o test && ./test'
--- computer-systems/intro-c/hashmap.c
+++ computer-systems/intro-c/hashmap.c
@@ -1,7 +1,80 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define STARTING_BUCKETS 8
+#define MAX_KEY_SIZE 20
+/*
+hashmap -> a key, value datastructure
+dict = {}
+dict['a'] = 2
+hash the key and get a number -> hash % mod to store the value 
+collision -> if there is a collision, do open addressing
+open addressing -> find an empty spot (+1) and store the data there
+
+keys array
+val array
+hash the keys -> store the keys in there
+at the same time, store the values in the val array
+[0,1,2,3,4,5,6,7]
+
+what if they are all full?
+
+*/
+
+typedef struct Hashmap {
+void **key_buckets;
+void **val_buckets;
+int bucket_size;
+} Hashmap;
+
+Hashmap *Hashmap_new(){
+  Hashmap *h = malloc(sizeof(Hashmap));
+  h->bucket_size = STARTING_BUCKETS;
+  h->key_buckets = malloc(h->bucket_size * sizeof(void *));
+  h->val_buckets = malloc(h->bucket_size * sizeof(void *));
+  return h;
+}
+// returns hashed index given the string 
+unsigned int simple_hash_index(const char *str, int bucket_size) {
+    unsigned int hash = 0;
+    while (*str) {
+        hash += (unsigned char)(*str);
+        str++;
+    }
+    return hash % bucket_size;
+}
+
+
+
+void Hashmap_set(Hashmap * h, char *key, void * val_ptr){
+  // hash
+  unsigned int hash = simple_hash(key);
+  // get the index
+  unsigned int index = hash % h->bucket_size;
+  h->key_buckets[index] = key;
+  h->val_buckets[index] = val_ptr;
+
+}
+
+void Hashmap_get (Hashmap *h, char *key) {
+  unsigned int hash = simple_hash(key);
+  
+  if (h->key_buckets[index])
+
+}
+
+void Hashmap_delete(Hashmap *h, const char *keys){
+
+}
+
+void Hashmap_free(Hashmap *h) {
+
+}
+
+
+
+
 
 int main() {
   Hashmap *h = Hashmap_new();
