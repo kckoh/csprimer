1a59497c04a1b1980829d71ad8f2ee478da54bb0
KyungCheol (KC) Koh <43446639+kckoh@users.noreply.github.com>
2025-08-19 09:48:21 +0000

    implement basic hello world in assembly

--- computer-systems/assembly/hello.asm
+++ computer-systems/assembly/hello.asm
@@ -1,12 +1,17 @@
 global _start
 
 section .data
-
+    msg db 'Hello, Assembly!', 0x0a
+    len_msg equ $ - msg
 
 section .text
 _start:
-    ;mov rax, 0x01
-    ;syscall
+    ; write syscall to output
+    mov rax, 0x01
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, len_msg
+    syscall
     ; exit code with 0
     mov rax, 0x3c
     mov rdi, 0
