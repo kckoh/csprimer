364ddc00da01d0c8195b7bad77c16e8c18976803
KyungCheol (KC) Koh <43446639+kckoh@users.noreply.github.com>
2025-08-20 16:52:22 +0000

    implement the matrix assembly

--- computer-systems/assembly/matrix-access/matrix.asm
+++ computer-systems/assembly/matrix-access/matrix.asm
@@ -6,4 +6,10 @@ index:
 	; edx: cols
 	; ecx: rindex
 	; r8d: cindex
+	; calculation -> cols(rax) * rindex(ecx) + cindex
+	mov eax, edx
+	imul eax, ecx
+	add eax, r8d
+
+	mov rax, [rdi + rax*4]
 	ret
