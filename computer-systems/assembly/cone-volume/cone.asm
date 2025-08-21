3dc51b3e106851ecf0e289ec7d9a1a01a5aacc14
KyungCheol (KC) Koh <43446639+kckoh@users.noreply.github.com>
2025-08-21 18:32:48 +0000

    init fib assembly with stack

--- computer-systems/assembly/cone-volume/cone.asm
+++ computer-systems/assembly/cone-volume/cone.asm
@@ -1,6 +1,13 @@
 default rel
 
+section .data
+pi: dd 3.14
+third: dd 0.33
+
+
 section .text
 global volume
+;1/3 * pi * r^2 * h
 volume:
+
  	ret
--- computer-systems/assembly/cone-volume/tests.c
+++ computer-systems/assembly/cone-volume/tests.c
@@ -7,7 +7,7 @@
 extern float volume(float radius, float height);
 
 int main(void) {
-  assert(float_near(0.0f, volume(0.0f, 0.0f)));
+  // assert(float_near(0.0f, volume(0.0f, 0.0f)));
   assert(float_near(2.09f, volume(1.0f, 2.0f)));
   assert(float_near(174.23f, volume(5.5f, 5.5f)));
   assert(float_near(9.05f, volume(1.234f, 5.678f)));
