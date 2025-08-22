d98a1c387470358cd6e68af354f18a0ca166e4a1
KyungCheol (KC) Koh <43446639+kckoh@users.noreply.github.com>
2025-08-22 12:50:27 +0000

    temp save byte

--- os/bytewrite.c
+++ os/bytewrite.c
@@ -1,6 +1,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main() {
     // open requires three args: path, flags and mode
@@ -9,8 +11,18 @@ int fd = open("hello.txt", O_WRONLY | O_CREAT , 0644);
         // handle error
         return 1;
     }
-    printf("hello world");
-    write(fd, "Hello World\n", 12);
+    struct stat st;
+    if (stat("hello.txt", &st) ==-1){
+        return 1;
+    }
+
+    blksize_t init_blksize = st.st_blksize;
+    
+    while (st.st_blksize <= 1,000,000) {
+        stat("hello.txt", &st);
+
+    }
+    // printf("File size: %ld bytes\n", st.st_blksize);
     close(fd);
     return 0;
 }
