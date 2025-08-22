63ba3db9fc34b829f7d7e672f4fbc9241fe87d92
KyungCheol (KC) Koh <43446639+kckoh@users.noreply.github.com>
2025-08-22 12:06:46 +0000

    temp save to the byte write in os

--- os/README.md
+++ os/README.md
@@ -1,1 +1,10 @@
+## running a c program
 ls bytewrite.c | entr sh -c 'gcc bytewrite.c -o bytewrite && ./bytewrite'
+
+## generate a random bytes to a file 
+head -c 4097 /dev/urandom > hello.txt
+
+
+what i don't know
+block
+sector
--- os/bytewrite.c
+++ os/bytewrite.c
@@ -1,5 +1,16 @@
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
-int main(){
-    printf("Hello world!\n");
+
+int main() {
+    // open requires three args: path, flags and mode
+int fd = open("hello.txt", O_WRONLY | O_CREAT , 0644);
+    if (fd == -1) {
+        // handle error
+        return 1;
+    }
+    printf("hello world");
+    write(fd, "Hello World\n", 12);
+    close(fd);
     return 0;
 }
